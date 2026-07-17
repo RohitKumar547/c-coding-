72ea8754ea74fbd03dde2ac47f4037e4efe20697
RohitKumar547 <rohitkumar6487888@gmail.com>
2026-07-17 11:42:54 +0530

    first c program

---
+++ main1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    float gpa = 2.5;
+    float price = 19.19;
+    float temp =-4.55;
+    int age = 25;
+    int year=2026;
+    int quantity = 2;
+    printf("here the temperature is %.2f\n",temp);
+    printf("you gpa is %.2f\n ",gpa);
+    printf("the price is $%.2f°F\n",price );
+    printf("you are %d years old\n",age );
+    printf("this is %d\n",year);
+    printf("you ordered %d x item\n",quantity);
+    return 0;
+}
