27e78cc0c0245178369ae93c17f82a99301ea42b
RohitKumar547 <rohitkumar6487888@gmail.com>
2026-07-17 11:56:22 +0530

    its my second program

---
+++ main2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    double pi =3.1415976;
+    double e = 2.442666643;
+    char grade = 'f';
+    char symbol = '?';
+    printf("your fav symbol %c\n",symbol);
+    printf("your grade is %c\n",grade);
+    printf("%f is the value of pi\n",pi);
+    printf("%f is the value of e\n",e);
+
+    return 0;
+}
