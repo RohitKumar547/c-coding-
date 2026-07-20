a40a0ae1f839ecfc56196eb3383d13c534fbfa38
RohitKumar547 <rohitkumar6487888@gmail.com>
2026-07-20 23:09:04 +0530

    while but only2

---
+++ while.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main() {
+    while(1==1) {
+        printf("This is an infinite loop.");
+    }
+    return 0;
+}
---
+++ while2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+
+    int numb=0;
+
+    while(numb<=0){
+        printf("enter the number");
+        scanf("%d",&numb);
+    }
+    return 0;
+}
